ce6c08f565a1a321bfbc379d2bef7bd692824f73
leorev01 <eric@example.com>
2024-09-20 21:22:22 +0100

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +28,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <ClerkProvider appearance={{
+        layout:{
+          logoImageUrl: '/icons/yoom-logo.svg',
+          socialButtonsVariant: 'iconButton',
+        },
+        variables:{
+          colorText: '#fff',
+          colorPrimary: '#0E78F9',
+          colorBackground: '#1c1f2e',
+          colorInputBackground: '#252a41',
+          colorInputText: '#fff',
+        }
+      }}>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,56 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  	extend: {
+  		colors: {
+			dark: {
+				1: '#1C1F2E',
+				2: '#161925',
+				3: '#252A41',
+				4: '#1E2757',
+			  },
+			  blue: {
+				1: '#0E78F9',
+			  },
+			  sky: {
+				1: '#C9DDFF',
+				2: '#ECF0FF',
+				3: '#F5FCFF',
+			  },
+			  orange: {
+				1: '#FF742E',
+			  },
+			  purple: {
+				1: '#830EF9',
+			  },
+			  yellow: {
+				1: '#F9A90E',
+			  },
+		},
+		backgroundImage: {
+			hero: "url('/images/hero-background.png')",
+		  },
+		keyframes: {
+			'accordion-down': {
+			  from: { height: '0' },
+			  to: { height: 'var(--radix-accordion-content-height)' },
+			},
+			'accordion-up': {
+			  from: { height: 'var(--radix-accordion-content-height)' },
+			  to: { height: '0' },
+			},
+		  },
+  			
+	},
+	},
+  plugins: [tailwindcssAnimate],
 };
 export default config;
